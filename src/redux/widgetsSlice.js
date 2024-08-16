b688303897f17aee7175540caa66ee56bcb4a25b
Santhosh Jayavelu <127133708+SanthoshJayavelu22@users.noreply.github.com>
2024-08-16 09:30:07 +0530

    Update widgetsSlice.js

--- src/redux/widgetsSlice.js
+++ src/redux/widgetsSlice.js
@@ -1,11 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import data from '../widgetData.json';
 
-const initialWidgets = [
-  { id: 1, name: 'Security Overview', content: 'Security Overview content...' },
-  { id: 2, name: 'Compliance Status', content: 'Compliance Status content...' },
-  { id: 3, name: 'Resource Summary', content: 'Resource Summary content...' },
-];
+
 
 const widgetsSlice = createSlice({
   name: 'widgets',
