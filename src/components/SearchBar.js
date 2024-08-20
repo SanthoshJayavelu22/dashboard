eb9f824fbb8b3b198cc5f9b04715bbc4745f9e16
Santhosh Jayavelu <127133708+SanthoshJayavelu22@users.noreply.github.com>
2024-08-20 06:27:03 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,19 @@
+import React, { useState } from 'react';
 import React from 'react';
 
 const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
 
   const handleSearch = (e) => {
-  
+    setQuery(e.target.value);
+
     onSearch(e.target.value);
   };
 
   return (
     <input
       type="text"
+      value={query}
       onChange={handleSearch}
       placeholder="Search Widgets..."
       className="w-full p-2 mb-4 border rounded-md dark:bg-gray-600 "
